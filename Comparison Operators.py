106f74f24df47e17cd80c0ac8206593fe436470f
Kranthi <kranthi.anu4@gmail.com>
2021-10-03 17:55:20 +0530

    Create Comparison Operators.py

---
+++ Comparison Operators.py
@@ -0,0 +1,16 @@
+				Conditional Execution
+   Comparison Operators	
+Boolean expressions ask a question and produce a Yes or No result which we use to control program flow
+Boolean expressions using comparison operators evaluate to True / False or Yes / No
+Comparison operators look at variables but do not change the variables
+	Python      Meaning
+	<           Less than
+	<=          Less than or Equal to 
+	==          Equal to
+	>=          Greater than or Equal to
+	>           Greater than
+	!=          Not equal
+	Remember: "=" is used for assignment.
+	
+	
+   
